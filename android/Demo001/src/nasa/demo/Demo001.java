2d704c348e659644c4d58093d5d36b100c906912
Zack Moratto <zachary.m.moratto@nasa.gov>
2012-02-14 15:30:35 -0800

    Added Demo001 skeleton
    This is for the upcoming Demo next week.

---
+++ android/Demo001/src/nasa/demo/Demo001.java
@@ -0,0 +1,15 @@
+package nasa.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Demo001 extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
